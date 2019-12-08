6ba5ec5f43fe4c7fb6f8b6e92c34e9fc969277ff
nullreturner <nullreturner@gmail.com>
2019-12-08 21:18:38 +0900

    crlf adjusted

--- rocket_punch.py
+++ rocket_punch.py
@@ -2,7 +2,6 @@ import os
 os.system('cls')
 from selenium import webdriver
 from bs4 import BeautifulSoup
-# from bs4 import element as ele
 from bs4 import element
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -10,50 +9,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from pprint import pprint
 
-chromeOptions = Options()
-# chromeOptions.add_argument("--kiosk")
-chromeOptions.add_argument("headless")
-driver = webdriver.Chrome(options=chromeOptions)
+def main():
+    chromeOptions = Options()
+    chromeOptions.add_argument('headless')
+    driver = webdriver.Chrome(options=chromeOptions)
 
-# 파싱할 페이지로 이동
-driver.get("https://www.rocketpunch.com/jobs?job=sw-developer")
-WebDriverWait(driver, 10).until(expected_conditions.presence_of_all_elements_located(By.ID, "fb-root"))
+    driver.get('https://www.rocketpunch.com/jobs?job=sw-developer')
+    # WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, 'fb-root')))
 
-# driver.get("https://www.rocketpunch.com/jobs")
+    companies = []
+    for item in BeautifulSoup(driver.page_source, 'html.parser').find_all(attrs={'class': 'company item'}):
+        company = {}
+        companyName = item.find(attrs={'class': 'header name'}) \
+                    .find(lambda x : 
+                        not isinstance(x, element.NavigableString)
+                        and x.name == 'strong').text
+        company['companyNm'] = companyName
 
-# BeautifulSoup으로 html 파싱
-companies = []; companies
-for item in BeautifulSoup(driver.page_source, 'html.parser').find_all(attrs={"class" : "company item"}):
+        info = {}
+        companySectors = item.find(attrs={'class': 'nowrap meta'}).get_text().strip()
+        info['company-sector'] = companySectors
 
-    #회사명/업종명
-    company = {}; company
-    companyName = item.find(attrs={"class":"header name"}) \
-                .find(lambda x : 
-                        not isinstance(x, element.NavigableString) 
-                        and x.name == 'strong').text; companyName # 회사명
-    company['companyNm'] = companyName; company
+        company[companyName] = info
 
-    info = {}
-    companySectors = item.find(attrs={"class": "nowrap meta"}).get_text().strip(); companySectors # 업종명
-    # company[companyName]['company-sector'] = companySectors
-    info['company-sector'] = companySectors; info
+        jobs = []
+        for desc in item.find(attrs={'class': 'company-jobs-detail'}).descendants:
+            job = {}
+            if not isinstance(desc, element.NavigableString) and 'job-detail' in desc.get_attribute_list('class'):
+                job['job-title'] = desc.find(attrs={'class': 'job-title'}).text
+                job['job-stat-info'] = desc.find(attrs={'class': 'job-stat-info'}).text
+            job and jobs.append(job)
 
-    company[companyName] = info; company
+        info['jobs'] = jobs
+        company['name'] = companyName
+        company['info'] = info
+        companies.append(company)
+        driver.quit()
 
-    jobs = [] 
-    for desc in item.find(attrs={"class": "company-jobs-detail"}).descendants:
-        job = {}
-        if not isinstance(desc, element.NavigableString) and 'job-detail' in desc.get_attribute_list('class'):
-            job['job-title'] = desc.find(attrs={"class": "job-title"}).text; job
-            job['job-stat-info'] = desc.find(attrs={"class": "job-stat-info"}).text; job
-        job and jobs.append(job); jobs
+    pprint(companies)
 
-    info['jobs'] = jobs; info
-    company['name'] = companyName
-    company['info'] = info; company
-    companies.append(company); # pprint(companies)
-
-    # 수집된 게시물을 담을 자료구조
-    # 페이징 처리된 태그와 속성값을 확인 후 다음 번에 이동할 페이지를 저장하기
-
-pprint(companies)
+if __name__ == '__main__':
+    main()
