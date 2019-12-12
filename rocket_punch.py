7b73b2e874900e91a2df0f29139a892127a8b0ed
nullreturner <yoonsung0711@kw.ac.kr>
2019-12-12 17:26:29 +0900

    amended data structure

--- rocket_punch.py
+++ rocket_punch.py
@@ -20,45 +20,61 @@ def driverGet():
 
     while True:
         if driver.page_source[-7:] == "</html>":
+            print('page loaded')
             time.sleep(0.0001)
             items = BeautifulSoup(driver.page_source, 'html.parser').find_all(attrs={'class':'company item'})    
             if  items != []:
+                print('items ready')
                 break
+
         yield from asyncio.sleep(0.5)
+
+    # companies = []
+        # company = {}
+            # company['name'] = ''
+            # company['sector'] = ''
+            # company['jobs'] = [] 
+                # job = {}
+                    # job['title'] = ''
+                    # job['career'] = ''
+                    # job['link'] = ''
+                    # job['valid_until'] = ''
+                    # job['modified'] = ''
+
 
     companies = []
     for item in items:
-        company = {}
-        companyName = item.find(attrs={'class': 'header name'}) \
+        name = item.find(attrs={'class': 'header name'}) \
             .find(lambda x : 
                 not isinstance(x, element.NavigableString)
                 and x.name == 'strong').text
+        sector = item.find(attrs={'class': 'nowrap meta'}).get_text().strip()
 
-    info = {}
-    companySectors = item.find(attrs={'class': 'nowrap meta'}).get_text().strip()
-    info['company-sector'] = companySectors
+        company = {}
+        company['name'] = name # 회사명
+        company['sector'] = sector # 업종
 
-    company[companyName] = info
+        jobs = []
+        company['jobs'] = jobs # 채용
 
-    jobs = []
-    for desc in item.find(attrs={'class': 'company-jobs-detail'}).descendants:
-        job = {}
-        if not isinstance(desc, element.NavigableString) and 'job-detail' in desc.get_attribute_list('class'):
-            job['job-title'] = desc.find(attrs={'class': 'job-title'}).text
-            job['job-stat-info'] = desc.find(attrs={'class': 'job-stat-info'}).text
-        job and jobs.append(job)
+        for desc in item.find_all(attrs={'class': 'job-detail'}):
+            if not isinstance(desc, element.NavigableString) and 'job-detail' in desc.get_attribute_list('class'):
+                job = {}
+                job['title'] = desc.find(attrs={'class': 'job-title'}).text
+                job['career'] = desc.find(attrs={'class': 'job-stat-info'}).text
+                job['link'] = desc.a['href']
+                job and jobs.append(job)
 
-    info['jobs'] = jobs
-    company['name'] = companyName
-    company['info'] = info
-    companies.append(company)
-    driver.quit()
+        companies.append(company)
+        pprint(company)
+
+        driver.quit()
+
     json_data = json.dumps(companies, ensure_ascii=False)
-
     with open('.\\companies.json', 'w', encoding='utf-8') as jsonfile:
         json.dump(json_data, jsonfile, ensure_ascii=False)
     # pprint(companies)
-
+    pprint(companies)
 
 def main():
     loop = asyncio.get_event_loop()
